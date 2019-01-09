612c56b176fc91e8b6828c9fe896ca458061aa84
EbonyToussaint <46454053+EbonyToussaint@users.noreply.github.com>
2019-01-09 10:17:46 -0500

    Add files via upload

--- day02_homework_EThomework.R
+++ day02_homework_EThomework.R
@@ -12,13 +12,23 @@ NCS -> National_Congregations_Study_Panel_Dataset_1998_and_2006_2007_
 
 View(NCS)
 
+#what professor did
+head(NCS)
+
 # 3. Peek at the top few rows for only a few columns
 
 #wouldn't this be the same as question 2???
 
+#what professor did
+head(NCS[, 1:5])
+
 # 4. How many rows does your data have?
 
 #262
+
+#what professor did
+nrow(NCS)
+length(NCS) # this gives a different answer
 
 # 5. Get a summary for every column
 
@@ -29,6 +39,11 @@ summary(NCS)
 colnames(NCS)
 NCS$TRAD3_06 #religious tradition
 
+#what professor did
+
+summary(NCS$TRAD3_06) # compare with what you get with:
+summary(NCS[, c("TRAD3_06", "AllMinPairs")])
+
 # 7. Are any of the columns giving you unexpected values?
 
 #The min for some columns are -3, which doesn't make sense for
@@ -38,10 +53,18 @@ NCS$TRAD3_06 #religious tradition
 #However, the values for religious tradition makes sense, as there
 #are only 6 values possible.
 
+#what professor did
+summary(NCS$AllMinPairs)
+
 # 8. Select a few key columns, make a vector of the column names
 x -> TRAD3_06
 y -> HLTHED06
 z -> APPGOV06
+
+#what professor did
+colnames(NCS)
+columns.to.examine <- c("TRAD3_06", "Pair", "WithinMinPairs", "Merged")
+head(NCS[, columns.to.examine])
 
 # 9. Create a new data.frame with just that subset of columns
 #not sure what I'm really supposed to do here. I'm not grasping
@@ -63,6 +86,9 @@ colnames(NCS)
 NCS[2]
 NCS[1:5]
 
+#what professor did
+NCS.sub1 <- NCS[, columns.to.examine]
+
 # 10. Create a new data.frame that is just the first 10 rows
 #     and the last 10 rows of the data from the previous step
 
@@ -72,6 +98,13 @@ NCS[1:5]
 tail(NCS,10)
 head(NCS,10)
 
+#what professor did
+nrow(NCS.sub1)
+NCS.sub1.topbottom <- NCS.sub1[c(1:10, 625:634), ]
+NCS.sub1.topbottom
+# a better way:
+NCS.sub1.topbottom <- NCS.sub1[c(1:10, (nrow(NCS.sub1) - 9):nrow(NCS.sub1)), ]
+
 # 11. Create a new data.frame that is a random sample of half of the rows.
 # HINT: ?sample
 
@@ -79,6 +112,11 @@ head(NCS,10)
 sample_n(TRAD3_06, 10)
 #I got error message that tbl must be a data frame
 #I guess that's what I missed in questions 9 and 10
+
+#what professor did
+NCS.sub2 <- NCS[sample(nrow(NCS), replace = FALSE), ] # good to be explicit here!
+rownames(NCS)
+rownames(NCS.sub2)
 
 # 12. Find a comparison in your data that is interesting to make
 #     (comparing two sets of numbers)
@@ -94,6 +132,10 @@ x <- CLERAG06
 y <- CLERAG98
 
 t.test(x, y)
+
+#what professor did
+first.test <- t.test(WithinMinPairs ~ Merged, data = NCS)
+first.test
 
 # 13. Repeat #12 for TWO more comparisons
 #     - Tip: it's okay if the comparisons are kind of nonsensical, this is 
@@ -111,6 +153,13 @@ y <- SERMTM06
 t.test(x, y)
 
 # 14. Save all results from #12 and #13 in an .RData file
+
+#what professor did
+save(first.test, file = "first t-test results.RData")
+rm(first.test)
+first.test
+load("first t-test results.RData")
+first.test
 
 # 15. Email me your version of this script, PLUS the .RData
 #     file from #14
